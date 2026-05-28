41fd20c4a7abbec765f0a3ee6bbd558f1274eac1
Omar-FD <s4165749@student.rmit.edu.au>
2026-05-28 23:02:03 +1000

    Feature(Implement DriverId addition function to verify the id before inserting)

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -1,6 +1,8 @@
 package driver;
 
 import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Driver {
     enum LICENSE_TYPES {
@@ -22,6 +24,24 @@ public class Driver {
         return driverID.equals(this.driverID) && this.birthdate.equals(LocalDate.parse(date));
     }
     public void addID(String driverID) {
+        //checking length
+        if (driverID.length() < 10) {
+            return;
+        }
+        //checking structure
+        String firstTwo = driverID.substring(0,2);
+        if (!firstTwo.matches("[2-9]{2}")) {
+            return;
+        }
+        String middleFive = driverID.substring(2,9);
+        long specialCount = middleFive.chars().filter(c -> !Character.isLetterOrDigit(c)).count();
+        if (specialCount != 2) {
+            return;
+        }
+        String lastTwo = driverID.substring(9,11);
+        if (!lastTwo.matches("[A-Z]{2}")) {
+            return;
+        }
         this.driverID = driverID;
     }
 }
