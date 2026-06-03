57222df9220c6d6b93fab375da8af85184fa7c52
YahyeMohamed2323 <s4172313@student.rmit.edu.au>
2026-06-04 08:41:34 +1000

    update my work to work with the bus.java class. and aded .gitignore file

---
+++ src/test/java/bus/BusIntegrationTest.java
@@ -0,0 +1,115 @@
+
+package bus;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import driver.Bus;
+
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class BusIntegrationTest {
+
+    //private BusRepository busRepository;
+
+    
+  
+     //IT-B01: Add a valid bus and verify it can be retrieved from the TXT file.
+   
+    @Test
+    @Order(1)
+    void ValidBusesStoredCorrectly() {
+        BusRepository repository = new BusRepository();
+        
+        Bus bus = new Bus("12345678", 40, 80.0, "Diesel");
+        boolean result = repository.add(bus);
+
+        assertTrue(result);
+        assertEquals(1, repository.count());
+    }
+
+
+
+
+
+
+    //IT-B02: Add multiple valid buses and confirm all are persisted.
+    @Test
+    @Order(2)
+    void InvalidBusesAreRejected() {
+
+        BusRepository repository = new BusRepository();
+
+        try {
+            Bus bus = new Bus("INVALID", 30, 60.0, "Diesel");
+            boolean result = repository.add(bus);
+            assertFalse(result);
+        } catch (IllegalArgumentException e) {
+            
+            assertEquals(0, repository.count());
+        }
+       
+
+    }
+
+    
+
+
+
+
+
+     //IT-B03: Attempt to add a bus with a duplicate busID – must be rejected.
+   
+    @Test
+    @Order(3)
+    void UpdatesPersistedCorrectly() {
+
+        BusRepository repository = new BusRepository();
+
+        Bus bus = new Bus("55555555", 40, 60.0, "Diesel");
+        repository.add(bus);
+
+        Bus updated = new Bus("55555555", 40, 95.0, "Hybrid");
+        boolean result = repository.update(updated);
+
+        assertTrue(result);
+        assertEquals(1, repository.count());
+    }
+
+
+
+
+
+
+
+
+
+
+
+    //IT-B08: Count starts at 0, increments with each valid add,
+   
+    @Test
+    @Order(6)
+    void CountsUpdatedCorrectly() {
+        
+        BusRepository repository = new BusRepository();
+        repository.add(new Bus("10000001", 30, 60.0, "Diesel"));
+        assertEquals(1, repository.count());
+
+        repository.add(new Bus("10000002", 45, 80.0, "Hybrid"));
+        assertEquals(2, repository.count());
+
+        try {
+            repository.add(new Bus("INVALID", 20, 50.0, "Diesel"));
+        } catch (IllegalArgumentException e) {
+            
+        }
+        assertEquals(2, repository.count());
+    }
+
+}
