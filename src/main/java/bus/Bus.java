4f3aed36acfbabc22088073b20740630d808be24
Tuan_Kiet Le <s41140318@student.rmit.edu.au>
2026-06-04 14:19:30 +1000

    relocate file

--- src/main/java/bus/Bus.java
+++ src/main/java/bus/Bus.java
@@ -1,4 +1,4 @@
-package driver;
+package bus;
 
 import java.time.LocalDate;
 import java.time.Period;
--- src/test/java/bus/BusTest.java
+++ src/test/java/bus/BusTest.java
@@ -1,4 +1,4 @@
-package driver;
+package bus;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
